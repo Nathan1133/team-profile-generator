a5cfb724745dfd72e61212c6b8e0619145cebadc
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-02-06 18:39:18 -0500

    Created/Added to remaining classes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,9 @@
 const employee = require('../lib/Employee')
 
 test("employee ", () => {
+
+    const employee = new employee("Nate","fake@test.com","2");
+    
     expect(employee.name).toequal("Nate");
     expect(employee.email).toequal("fake@test.com");
     expect(employee.id).toequal("2");
